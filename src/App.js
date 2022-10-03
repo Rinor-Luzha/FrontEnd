d82acea6a767f46d1c4706ce713a3f8175311bc5
Rinor Luzha <rl51842@ubt-uni.net>
2022-10-03 18:58:46 +0200

    FrontEnd setup

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect } from 'react';
+import { urlName } from './endpoints';
+function App() {
+  return <h1>Hello</h1>;
+}
+
+export default App;
