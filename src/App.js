892a2c13c2a615b72a4d4e53f127bc6e1f6d5e07
dijamantferataj0 <dijamant.ferataj@life.gjirafa.com>
2022-10-05 23:04:28 +0200

    Header UI Complete

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import React, { useEffect } from 'react';
 import { urlName } from './endpoints';
-import Header from './Header';
+import Header from './header';
 
 function App() {
   return (
     <div className="container">
-
+      <Header/>
     </div>
   )
 }
--- src/header.js
+++ src/header.js
@@ -10,8 +10,10 @@ export default function Header() {
 
   return (
     <>
-      <div className='flex bg-white h-15 py-1 shadow-md'>
+      <div className='space-x-9 flex justify-items-center items-center bg-white h-15 py-1 shadow-lg'>
         <MenuButton/>
+        <SearchBar/>
+        <Logo/>
       </div>
     </>
   )
@@ -27,3 +29,22 @@ function MenuButton(){
   )
 }
 
+
+
+function SearchBar(){
+  return(
+    <div className=" text-gray-600">
+      <input type="search" name="serch" placeholder="Search..." className=" bg-gray-200 h-10 py-1 px-1 ml-0 pl-5 mr-1 pr-10 rounded-full text-sm focus:outline-none"></input>
+      <button type="submit" class="absolute top-0.5 left-21 mt-3 h-7 w-7 bg-[url('/public/searchicon.png')] bg-contain bg-no-repeat bg-right">
+    
+      </button>
+    </div>
+  )
+}
+
+function Logo(){
+  return(
+    <div className="bg-contain bg-[url('/public/logo.png')] bg-no-repeat h-10 w-10">
+    </div>
+  )
+}
