552f531326b852318f4018ee97d9f97881b2d42c
Donjete Tifeku <donjete.tifeku@life.gjirafa.com>
2022-10-07 20:59:03 +0200

    Create ActorsList.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Footer from '../components/Footer' 
+import MainSlider from '../components/sliders/main/MainSlider'
+import Actors from '../components/ActorList'
+import ListItem from '../components/ListItem'
 
 export default function Home() {
   return (
-    <>
-    </>
+    <div>
+      <ListItem />
+    <MainSlider />
+    <Footer />
+    </div>
   )
 }
