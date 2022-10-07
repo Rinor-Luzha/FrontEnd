d57924e19a5cdd74f7b2bf144486017b5417761d
Rinor Luzha <rl51842@ubt-uni.net>
2022-10-08 00:50:49 +0200

    Add main slider

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,52 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Slide from '../components/slides/BigSlide'
+import AliceCarousel from 'react-alice-carousel';
 
-export default function Home() {
+export const getStaticProps = async () => {
+  const res = await fetch('http://localhost:39249/home/new');
+  const data = await res.json();
+  return {
+    props: { movieList: data }
+  }
+}
+
+
+
+export default function Home({ movieList }) {
   return (
     <>
+      <div className="w-screen h-104 bg-black absolute shadow-xl"></div>
+      <AliceCarousel duration={500}
+        autoPlay={true}
+        startIndex={1}
+        fadeOutAnimation={true}
+        mouseDragEnabled={true}
+        playButtonEnabled={true}
+        autoPlayInterval={10000}
+        touchTracking={true}
+        autoPlayActionDisabled={true}
+        renderPrevButton={() => {
+          return <p className="left-5 px-[12px] pt-[3px] inline-block bottom-4 text-grey rounded-full border-2 border-grey text-4xl hover:cursor-pointer hover:bg-red hover:shadow-md hover:text-white hover:border-white transition-all font-bold">&lt;</p>
+        }}
+        renderNextButton={() => {
+          return <p className="left-5 px-[12px] pt-[3px] inline-block bottom-4 text-grey rounded-full border-2 border-grey text-4xl hover:cursor-pointer hover:bg-red hover:shadow-md hover:text-white hover:border-white transition-all font-bold">&gt;</p>
+        }}>
+        {
+          movieList.map(movie => {
+            const genreArray = movie.genres;
+            console.log(genreArray);
+            let genres = "";
+            genreArray.map(genre => {
+              genres += genre + ", "
+            })
+
+            return <Slide title={movie.title}
+              genres={genres.substring(0, genres.length - 2)}
+              img={movie.img.substring(1)}
+            />
+          }
+          )
+        }
+      </AliceCarousel>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,25 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '104': '28rem',
+      }
+    },
+    colors: {
+      'red': '#e20100',
+      'white': '#FFFFFF',
+      'black': '#191818',
+      'grey': '#aaacb1'
+    },
+    fontWeight: {
+      fontWeight: {
+        normal: 400,
+        medium: 500,
+        semibold: 600,
+        bold: 900,
+      }
+    }
   },
   plugins: [],
 }
