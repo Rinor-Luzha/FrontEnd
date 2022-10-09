e898f54898318f48219912c2b2385ed6ce6f0835
Donjete Tifeku <donjete.tifeku@life.gjirafa.com>
2022-10-09 03:13:24 +0200

    Finish footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,38 +1,40 @@
 import React from "react";
+import { FaFacebookF,FaInstagram,FaLinkedin,FaTwitter } from 'react-icons/fa'
+import Image from 'next/image'
 
 function Footer() {
   return (
-    <div className="main-footer">
-      <div className="container">
-        <div className="row">
+    <div className="main-footer bg-black fixed inset-x-0 bottom-0">
 
-          <h1>Elefanti Ratings</h1>
-
-          <div className="col">
-            {/* Column1 */}
-            <h4>FIND US ON</h4>
-            <ul className="list-unstyled">
-              <li>Facebook</li>
-              <li>Twitter</li>
-              <li>Instagram</li>
-            </ul>
-            {/* Column2 */}
-            <h4>RESOURCES</h4>
-            <ul className="list-unstyled">
-              <li>Support</li>
-              <li>FAQ</li>
-            </ul>
-          </div>
+       <div className="flex justify-center my-4">
+        <a href="https://www.facebook.com/" className="border-2 border-white rounded-full p-4 mx-1 ">
+            <FaFacebookF className="text-md" />
+        </a>
+        <a href="https://www.instagram.com/" className="border-2 border-whitw rounded-full p-4 mx-1 ">
+            <FaInstagram className="text-md" />
+        </a>
+        <a href="https://twitter.com/" className="border-2 border-white rounded-full p-4 mx-1 ">
+            <FaTwitter className="text-md" />
+        </a>
+        <a href="https://www.linkedin.com/" className="border-2 border-white rounded-full p-4 mx-1 ">
+            <FaLinkedin className="text-md" />
+        </a>
         </div>
+
+        <div className="text-center justify-center font-bold flex items-center text-white text-2xl  xl:bg-black pt-2">
+            <div>
+                <Image src='/logo-removebg.png' width={90} height={70} />
+            </div>
+                <span className="text-red px-2">Elefanti</span>Ratings
+        </div>
+
         <hr />
         <div className="row">
-          <p className="col-sm">
-            &copy;{new Date().getFullYear()} Elefanti Ratings | All rights reserved | Terms Of Service | Privacy
+          <p className="col-sm text-center item-center p-2 text-grey">
+           Copyright &copy;{new Date().getFullYear()} | All rights reserved  
           </p>
         </div>
-        <div></div>
-      </div>
-    </div>
+    </div>    
   );
 }
 
