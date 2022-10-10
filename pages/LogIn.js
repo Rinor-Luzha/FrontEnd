666efc3f7d782760b38929f17d190c53b5eecb53
Donjete Tifeku <donjete.tifeku@life.gjirafa.com>
2022-10-10 19:55:37 +0200

    Add Login/Register validation.

--- pages/LogIn.js
+++ pages/LogIn.js
@@ -6,7 +6,6 @@ import { useState } from 'react'
 import { useRouter } from 'next/router'
 import swal from 'sweetalert';
 
-
 export default function Login() {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
@@ -21,50 +20,47 @@ export default function Login() {
 
     const login = async (e) => {
         e.preventDefault();
-        try {
-            const resUserLogin = await fetch('http://localhost:39249/account/login', {
-                method: "POST",
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                credentials: 'include',
-                body: JSON.stringify({ email, password })
+        const resUserLogin = await fetch('http://localhost:39249/account/login', {
+            method: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            credentials: 'include',
+            body: JSON.stringify({ email, password })
+        });
+        const userLogin = await resUserLogin.json();
+
+        if (userLogin.message === "Success") {
+            router.push('/')
+            swal({
+                title: "Good job!",
+                text: "Logged in!",
+                icon: "success",
+                timer: 1500,
+                buttons: false
             });
-            if (!resUserLogin.ok) {
-                throw new Error("Error while logging in!")
-            }
-            const userLogin = await resUserLogin.json();
+        } else {
+            setEmail("")
+            setPassword("")
+            setFail(true)
+        }
 
-            if (userLogin.message === "Success") {
-                router.push('/')
-                swal({
-                    title: "Good job!",
-                    text: "Logged in!",
-                    icon: "success",
-                    timer: 1500,
-                    buttons: false
-                });
-            } else {
-                setEmail("")
-                setPassword("")
-                setFail(true)
-            }
-
-            const emailInput = document.getElementById('email');
-            const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-
-            if (!emailInput.value.match(emailRegex)) {
-                alert('Invalid email address.');
-            }
-        } catch (error) {
-            console.log(error)
+        if (userLogin.message === "Invalid Credentials") {
+            router.push('/LogIn')
+            swal({
+                title: "Error!",
+                text: "Account doesnt exist or you entered wrong email or password!",
+                icon: "warning",
+                timer: 2500,
+                buttons: false
+            });
         }
     }
     return (
         <div className="flex flex-col items-center justify-center w-screen h-screen flex-1 px-20 text-center bg-white">
 
-            <div className="bg-white rounded-2xl shadow-2xl flex w-screen lg:w-2/3 lg:max-w-4xl flex-col lg:flex-row">
+            <div className="bg-white rounded-2xl shadow-2xl flex w-screen lg:w-2/3 lg:max-w-4xl md:max-w-xl flex-col lg:flex-row">
                 <div className="h-full lg:w-3/5 lg:p-5">
                     <div className="justify-center lg:justify-start lg:text-left font-bold flex items-center text-xl text-black">
                         <div>
@@ -78,7 +74,7 @@ export default function Login() {
                     </div>
                     <form className="flex flex-col items-center m-0" onSubmit={login}>
                         <div className="bg-lightgrey rounded-lg w-72 p-2 flex items-center m-0"><FaRegEnvelope className='text-grey m-2' />
-                            <input onKeyDown={handleKeyDown} onChange={e => { setEmail(e.target.value) }} type='email' name='email' placeholder='E-mail' id='email' required className='bg-lightgrey outline-none text-sm text-black flex-1' />
+                            <input onKeyDown={handleKeyDown} onChange={e => { setEmail(e.target.value) }} type='email' name='email' placeholder='E-mail'id='email' required className='bg-lightgrey outline-none text-sm text-black flex-1' />
                         </div>
                         <div className="bg-lightgrey rounded-lg w-72 p-2 flex items-center m-3"><MdLockOutline className='text-grey m-2' />
                             <input onKeyDown={handleKeyDown} onChange={e => { setPassword(e.target.value) }} type='password' name='password' placeholder='Password' className='bg-lightgrey outline-none text-sm text-black flex-1' />
@@ -92,7 +88,7 @@ export default function Login() {
                     <h2 className="text-3xl font-bold mb-2">First time here?</h2>
                     <div className="border-t-2 w-14 border-white inline-block mb-2"></div>
                     <p className="mb-10">Fill up personal information and start rating with us.</p>
-                    <Link href='/register'>
+                    <Link href='/Register'>
                         <button className="border-2 border-white rounded-full px-12 py-2 inline-block text-white font-semibold  hover:bg-white hover:text-red">Register</button>
                     </Link>
                 </div>
--- pages/Register.js
+++ pages/Register.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import Image from 'next/image'
+import AliceCarousel from 'react-alice-carousel';
 import { FaRegEnvelope } from 'react-icons/fa'
 import { MdLockOutline } from 'react-icons/md'
 import Link from 'next/link'
 import { useState } from 'react'
 import { useRouter } from 'next/router'
+import swal from 'sweetalert';
 
 export default function Register() {
     const [name, setName] = useState("")
@@ -28,57 +29,138 @@ export default function Register() {
     const register = async (e) => {
         e.preventDefault();
 
-        const password = document.getElementById('password').value;
-        const passwordConf = document.getElementById('passwordConf').value;
-        if (password?.value !== passwordConf?.value) {
-            alert('Entered passwords do not match');
+        if (name === '') {
+            swal({
+                title: "First Name is required!",
+                text: "Please enter your First Name!",
+                icon: "warning",
+                timer: 2000,
+                buttons: false
+               });
+              return    
         }
-        if (password.length <= 8) {
-            alert('Password must be 8 or more characters long')
-        }
-        // Add more validation
-        // if (password !== passwordConf) {
-        //     return
-        // }
 
-        try {
-            const resUserRegister = await fetch('http://localhost:39249/account/register', {
-                method: "POST",
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ name, surname, email, birthDate, password })
+        if (name.length <= 1) {
+            swal({
+                title: "Invalid name!",
+                text: "First Name must have more then 1 character!",
+                icon: "warning",
+                timer: 2000,
+                buttons: false
+               });
+              return    
+        }
+
+        if (surname === '') {
+            swal({
+                title: "Last Name is required!",
+                text: "Please enter your Last Name!",
+                icon: "warning",
+                timer: 2000,
+                buttons: false
+               });
+              return    
+        }
+
+        if (surname.length <= 1) {
+            swal({
+                title: "Invalid Last name!",
+                text: "Last Name must have more then 1 character!",
+                icon: "warning",
+                timer: 2000,
+                buttons: false
+               });
+              return    
+        }
+
+        if (birthDate === '') {
+            swal({
+                title: "Birthday Year is required!",
+                text: "Please enter your Birthday Year!",
+                icon: "warning",
+                timer: 2000,
+                buttons: false
+               });
+              return    
+        }
+       
+        if (birthDate > 2006) {
+            swal({
+                title: "Too young!",
+                text: "You have to be 16 years or older!",
+                icon: "warning",
+                timer: 2000,
+                buttons: false
+               });
+              return    
+        } 
+
+        if (password.length < 6 ) {
+            swal({
+                title: "Short Password!",
+                text: "Passwoord must have 6 or more characters!",
+                icon: "warning",
+                timer: 2000,
+                buttons: false
+               });
+              return    
+        } 
+
+        if (password !== passwordConf) {
+            swal({
+                  title: "Passwords do not match!",
+                  text: "You need to write the same passwoord as above!",
+                  icon: "warning",
+                  timer: 2400,
+                  buttons: false
+                 });
+                return
+            }
+        
+        // const regexp = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/i;
+        // if (regexp.test(password)) {
+        //     swal({
+        //           title: "Wrong type of password!",
+        //           text: "Password must contain at least one numeric digit, one uppercase and one lowercase letter!",
+        //           icon: "warning",
+        //           timer: 2000,
+        //           buttons: false
+        //         });
+        //         return
+        //     } 
+
+
+        const resUserRegister = await fetch('http://localhost:39249/account/register', {
+            method: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ name, surname, email, birthDate, password })
+        });
+        const userRegister = await resUserRegister.json();
+
+        if (resUserRegister.id !== "undefined") {
+            router.push('/LogIn')
+            swal({
+                title: "Good job!",
+                text: "Registered!",
+                icon: "success",
+                buttons: false
             });
-            if (!resUserRegister.ok) {
-                throw new Error("Error while registering user!");
-            }
-            const userRegister = await resUserRegister.json();
-
-            if (userRegister.id !== "undefined") {
-                router.push('/login')
-                swal({
-                    title: "Good job!",
-                    text: "Registered!",
-                    icon: "success",
-                    buttons: false
-                });
-            } else {
-                setName("")
-                setSurname("")
-                setEmail("")
-                setPassword("")
-                setBirthDate("")
-                setPasswordConf("")
-                setFail(true)
-            }
-        } catch (error) {
-            console.log(error);
+        } else {
+            setName("")
+            setSurname("")
+            setEmail("")
+            setPassword("")
+            setBirthDate("")
+            setPasswordConf("")
+            setFail(true)
         }
-    }
+    };
     return (
         <div className="flex flex-col items-center justify-center w-screen h-screen flex-1 lg:py-36 px-20 text-center bg-white">
-            <div className="bg-white rounded-2xl shadow-2xl flex w-screen h-screen lg:w-2/3 lg:max-w-4xl flex-col lg:flex-row">
+            <div className="bg-white rounded-2xl shadow-2xl flex w-screen h-screen lg:w-2/3 lg:max-w-4xl md:max-w-xl flex-col lg:flex-row">
                 <div className="h-1/5 lg:w-2/5 lg:p-5 lg:py-48 text-2xl">
                     <div className="font-bold flex-col items-center text-black">
                         <img src='/logo.png' className="w-35 h-20 pt-4 lg:w-50 lg:h-40 mx-auto" />
@@ -98,7 +180,7 @@ export default function Register() {
                         </div>
 
                         <div className="bg-lightgrey rounded-lg h-10 w-64 lg:h-10 lg:w-80 lg:p-2 flex items-center m-3">
-                            <input onChange={e => { setBirthDate(e.target.value) }} type='date' name='birthDate' placeholder='Birthday' className='bg-lightgrey outline-none text-sm text-black flex-1' />
+                            <input onChange={e => { setBirthDate(e.target.value) }} type='number' name='birthDate' placeholder='Birthday Year' className='bg-lightgrey outline-none text-sm text-black flex-1' />
                         </div>
 
                         <div onChange={e => { setEmail(e.target.value) }} className="bg-lightgrey rounded-lg h-10 w-64 lg:h-10 lg:w-80 lg:p-2 flex items-center m-3"><FaRegEnvelope className='text-grey m-2' />
@@ -106,11 +188,11 @@ export default function Register() {
                         </div>
 
                         <div onChange={e => { setPassword(e.target.value) }} className="bg-lightgrey rounded-lg h-10 w-64 lg:h-10 lg:w-80 lg:p-2 flex items-center m-3"><MdLockOutline className='text-grey m-2' />
-                            <input type="password" placeholder="Password" id="password" required className='bg-lightgrey outline-none text-sm text-black flex-1' />
+                        <input type="password" placeholder="Password" id="password" required className='bg-lightgrey outline-none text-sm text-black flex-1' />
                         </div>
 
                         <div onChange={e => { setPasswordConf(e.target.value) }} className="bg-lightgrey rounded-lg h-10 w-64 lg:h-10 lg:w-80 lg:p-2 flex items-center m-3"><MdLockOutline className='text-grey m-2' />
-                            <input type="password" placeholder="Confirm Password" id="passwordConf" required className='bg-lightgrey outline-none text-sm text-black flex-1' />
+                        <input type="password" placeholder="Confirm Password" id="passwordConf" required className='bg-lightgrey outline-none text-sm text-black flex-1' />
                         </div>
                         <div className={fail ? "text-red p-2 m-3 text-center text-xs" : "hidden"}>Invalid credentials!</div>
                         <button type="submit" className="border-2 border-white rounded-full px-12 py-2 mb-5 m-4 inline-block text-white font-semibold  hover:bg-white hover:text-red">Register</button>
